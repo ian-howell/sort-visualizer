564214bb49976cf09294c6750bcb6d4d2cd04478
Ian Howell <ian.howell0@gmail.com>
2016-10-13 10:32:12 -0500

    Add initial code for Column class

---
+++ column.cpp
@@ -0,0 +1,7 @@
+#include "column.h"
+
+void Column::draw(int x, int y)
+{
+    // Start at the (x, y) location and draw upward
+
+}
---
+++ column.h
@@ -0,0 +1,22 @@
+#ifndef COLUMN_H
+#define COLUMN_H
+
+class Column
+{
+    private:
+        int height;
+
+    public:
+        Column(int h) : height(h) {}
+        
+        /* draw
+         *
+         * draws the column to the screen
+         *
+         * Param x {int}: the horizontal location to start drawing from
+         * Param y {int}: the vertical location to start drawing from
+         */
+        void draw(int x, int y);
+};
+
+#endif
